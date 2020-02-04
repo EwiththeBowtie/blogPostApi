76e6a126709d62ec90da9c2d75b9bd47b840a11b
E Einowski <eeinowski@gmail.com>
2020-02-03 22:35:51 -0800

    Adds createPost

--- api/app/db.js
+++ api/app/db.js
@@ -3,15 +3,23 @@ const db = new sqlite3.Database("./db/blog.db", sqlite3.OPEN_READWRITE);
 
 const selectAllPostsQuery = "SELECT * FROM posts;";
 
+const insertPostQuery = () => `INSERT INTO posts (title,body) VALUES(?,?)`;
+
 const getAllPosts = () =>
   new Promise((resolve, reject) =>
     db.all(selectAllPostsQuery, (err, result) =>
-      err ? reject(err) : resolve(result)
+      err ? console.log(err) && reject(err) : resolve(result)
     )
   );
 
-const createPost = () => {};
+const createPost = (title, body) =>
+  new Promise((resolve, reject) =>
+    db.get(insertPostQuery(), [title, body], (err, result) =>
+      err ? console.log(err) && reject(err) : resolve(result)
+    )
+  );
 
 module.exports = {
-  getAllPosts
+  getAllPosts,
+  createPost
 };
--- api/app/index.js
+++ api/app/index.js
@@ -1,12 +1,35 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const Joi = require("@hapi/joi");
 const app = express();
 const db = require("./db");
-
+app.use(express.json());
 app.get("/posts", function(req, res) {
   db.getAllPosts().then(result => res.json(result));
 });
 
-app.post("/post", (req, res) => {});
+app.post("/post", (req, res) => {
+  const schema = Joi.object({
+    title: Joi.string()
+      .min(3)
+      .max(30)
+      .required(),
+
+    body: Joi.string()
+      .min(3)
+      .required()
+  });
+  const error = schema.validate(req.body).error;
+
+  if (error) {
+    res.status(400);
+    res.send(error);
+  } else {
+    db.createPost(req.body.title, req.body.body)
+      .then(result => res.status(201) && res.json(result))
+      .catch(error => res.status(400) && res.send(error));
+  }
+});
 
 app.listen(3000);
 console.log("Listening on port 3000...");
