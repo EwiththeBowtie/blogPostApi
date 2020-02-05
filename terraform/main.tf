1deac896a10c424520934e1788a2de31f2dca02c
E Einowski <eeinowski@gmail.com>
2020-02-04 23:13:47 -0800

    Removes leading spaces from json

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,18 +18,18 @@ resource "aws_iam_role" "codebuild" {
   name = "codebuildServiceRole"
 
   assume_role_policy = <<EOF
-			{
-				  "Version": "2012-10-17",
-					  "Statement": [
-							    {
-  						      "Effect": "Allow",
-							      "Principal": {
-   						        "Service": "codebuild.amazonaws.com"
-							      },
-							      "Action": "sts:AssumeRole"
-							    }
-					  ]
-			}
+{
+"Version": "2012-10-17",
+"Statement": [
+{
+"Effect": "Allow",
+"Principal": {
+"Service": "codebuild.amazonaws.com"
+},
+"Action": "sts:AssumeRole"
+}
+]
+}
 EOF
 }
 
