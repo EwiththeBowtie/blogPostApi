b5f68f0c80e49377301ceb07e0d50962b37c47a2
E Einowski <eeinowski@gmail.com>
2020-02-04 23:01:52 -0800

    Update to terraform 0.12 syntax

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,7 +39,7 @@ resource "aws_codebuild_project" "project" {
   build_timeout  = "5"
   queued_timeout = "5"
 
-  service_role = "${aws_iam_role.codebuild.arn}"
+  service_role = aws_iam_role.codebuild.arn
 
   artifacts {
     type = "NO_ARTIFACTS"
