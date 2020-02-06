46ae04651908a459d40b79c3c21c9966ca7162eb
E Einowski <eeinowski@gmail.com>
2020-02-06 01:58:25 -0800

    simplified db path

--- api/app/db.js
+++ api/app/db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("./db/blog.db", sqlite3.OPEN_READWRITE);
+const db = new sqlite3.Database("./blog.db", sqlite3.OPEN_READWRITE);
 
 const selectAllPostsQuery = "SELECT * FROM posts;";
 
